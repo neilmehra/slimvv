88b56723ff3c0dfdbe922df2b1d2ef540e5d9d6c
neil mehra <neilmehra@outlook.com>
2025-02-04 15:09:49 -0500

    rewriting aligned buffer handling

--- include/vv4.hpp
+++ include/vv4.hpp
@@ -1,65 +1,61 @@
 #pragma once
 
+#include <cassert>
+#include <concepts>
 #include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
 #include <utility>
 
-template <class T>
-concept bruh = requires(T a) {
-  { a + a } -> std::same_as<T>;
-};
-
+// replace type storage w/ bitmap that stores types using min # of types
+// so if we had variant<int, short, long> -> 00, 01, 10
+// and we had an int, short, long it would be
+// 000110 stored
 namespace vv5 {
 
-using dtor_fptr_t = void (*)(std::byte* const);
-using cm_fptr_t = void (*)(std::byte* const, const std::byte* const);
+namespace detail {
 
-template <class U> void destroy_impl(std::byte* const p) {
-  reinterpret_cast<U*>(p)->~U();
-}
-
-template <class U>
-void copy_impl(std::byte* const loc, const std::byte* const p) {
-  ::new (loc) U(*reinterpret_cast<const U* const>(p));
-}
-
-template <class U>
-void move_impl(std::byte* const loc, const std::byte* const p) {
-  ::new (loc) U(std::move(*reinterpret_cast<U*>(const_cast<std::byte*>(p))));
-}
-
-template <class... Types> class TypeBitmask {
-public:
-  void reserve(std::size_t bytes) {
-    std::byte* new_types = new std::byte[bytes];
-    for (std::size_t i = 0; i < bytes; i++) {
-      new_types[i] = types[i];
-    }
-    types = new_types;
-    delete[] new_types;
-  }
-
-private:
-  static constexpr std::size_t N = sizeof...(Types);
-  static constexpr std::size_t bnum = __builtin_clz(N);
-
-  static constexpr dtor_fptr_t dtable[N]{destroy_impl<Types>...};
-  static constexpr cm_fptr_t ctable[N]{copy_impl<Types>...};
-  static constexpr cm_fptr_t mtable[N]{move_impl<Types>...};
-
-  std::byte* types;
-
-  template <std::size_t I, class U, class T, class... TN>
-  [[nodiscard]] std::size_t find_type_index() const;
-};
-
-template <class... Types>
-template <std::size_t I, class U, class T, class... TN>
-std::size_t TypeBitmask<Types...>::find_type_index() const {
-  if constexpr (std::is_same_v<std::decay_t<U>, std::decay_t<T>>) {
+template <std::size_t I, class Q, class T, class... Rest>
+constexpr std::size_t find_type_in_pack() {
+  if (std::same_as<std::decay_t<Q>, std::decay_t<T>>) {
     return I;
   } else {
-    return find_type_index<I + 1, U, TN...>;
+    static_assert(sizeof...(Rest) > 0);
+    return find_type_in_pack<I + 1, Q, Rest...>();
   }
 }
 
+template <class T> constexpr std::size_t get_padding(std::uintptr_t addr) {
+  std::size_t align = alignof(T);
+  std::size_t aligned_addr = (addr + (align - 1)) & ~(align - 1);
+  return aligned_addr - addr;
+}
+
+// assumes there is enough space allocated at `addr`
+template <class T>
+constexpr std::size_t copy_to(std::uintptr_t* addr, const void* const p) {
+  std::uintptr_t u_addr = reinterpret_cast<std::uintptr_t>(addr);
+  std::size_t padding = get_padding<T>(addr);
+  const auto& data = *reinterpret_cast<const T* const>(p);
+  ::new (addr + padding) T(data);
+  return padding;
+}
+
+// assumes there is enough space allocated at `addr`
+template <class T>
+constexpr std::size_t move_to(std::uintptr_t* addr, void* const p) {
+  std::uintptr_t u_addr = reinterpret_cast<std::uintptr_t>(addr);
+  std::size_t padding = get_padding<T>(addr);
+  const auto& data = *reinterpret_cast<const T* const>(p);
+  ::new (addr + padding) T(std::move(data));
+  return padding;
+}
+
+template <std::size_t Size>
+constexpr std::size_t read_bits(const std::byte* const addr) {}
+
+} // namespace detail
 } // namespace vv5
